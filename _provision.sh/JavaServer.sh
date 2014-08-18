c085c44510f22e2280142da1e180e382200c76ae
PastLeo <chgu82837@gmail.com>
2014-08-18 22:48:50 +0800

    YESSSSSS!!! Successfully connect to mysql!!!

--- _provision.sh/JavaServer.sh
+++ _provision.sh/JavaServer.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-yum install -y tomcat tomcat-webapps tomcat-docs-webapp
+yum install -y tomcat tomcat-webapps tomcat-docs-webapp mysql-connector-java
+ln -s /usr/share/java/mysql-connector-java.jar /usr/share/tomcat/lib/
+echo "sleep 20" >> /etc/rc.local
+echo "service tomcat restart" >> /etc/rc.local
