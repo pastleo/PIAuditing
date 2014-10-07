1b5faef4de16cb1720ad816a13bfb95225d979e7
hsine <extraordinary1022@gmail.com>
2014-10-07 10:53:40 +0800

    model api

--- pia/src/main/java/pia/model/BaseModel.java
+++ pia/src/main/java/pia/model/BaseModel.java
@@ -1,20 +1,67 @@
 package pia.model;
+
 import java.sql.*;
+//import pia.model.DbInit;
 
 public abstract class BaseModel{
 	private static String tableName = "n/a";
-	private static PreparedStatement select;
-	private static PreparedStatement insert;
-	private static PreparedStatement update;
-	private static PreparedStatement delete;
+	private static String pk = "";
+	private static PreparedStatement all;
 
 	public static void init(){
-		//Connection conn from dbinit
-		select = conn.prepareStatement("select * from ?");
-		select.setString(1,tableName);
+		all = DbInit.getStatement("SELECT * from ?");
+		try{
+			all.setString(1,tableName);	
+		}catch(Exception e){}
 	}
+
 	public static ResultSet getAll(){
 		//Or do not return ResultSet
-		return select.executeQuery();
+		try{
+			return all.executeQuery();
+		}catch(Exception e){}
+		return null;
+	}
+
+	public static void delete(String id){
+		PreparedStatement del =
+			 DbInit.getStatement("DELETE FROM ? WHERE ?=?");
+		try{
+			del.setString(1,tableName);
+			del.setString(2,pk);
+			del.setString(3,id);
+			del.executeQuery();
+		}catch(Exception e){}		
+	}
+
+	public static void select(String id){
+		PreparedStatement sel =
+			 DbInit.getStatement("SELECT FROM ? WHERE ?=?");
+		try{
+			sel.setString(1,tableName);
+			sel.setString(2,pk);
+			sel.setString(3,id);
+			sel.executeQuery();
+		}catch(Exception e){}		
+	}
+
+	public static void update(String id){
+		PreparedStatement upd =
+			 DbInit.getStatement("UPDATE FROM ? SET ... WHERE ?=?");
+		try{			
+			upd.setString(1,tableName);
+			upd.setString(2,pk);
+			upd.setString(3,id);
+			upd.executeQuery();
+		}catch(Exception e){}		
+	}
+
+	public static void insert(){
+		PreparedStatement ins =
+			 DbInit.getStatement("INSERT INTO ? VALUES ...");
+		try{
+			ins.setString(1,tableName);
+			ins.executeQuery();
+		}catch(Exception e){}
 	}
 }
--- pia/src/main/java/pia/model/DB_base.java
+++ pia/src/main/java/pia/model/DB_base.java
@@ -14,9 +14,8 @@ public class DB_base {
         String pwd="";
 
         Class.forName("com.mysql.jdbc.Driver").newInstance(); 
-        //建立一個聯結物件 
+        
         return DriverManager.getConnection(url,user,pwd); 
     }
-
 
 }
